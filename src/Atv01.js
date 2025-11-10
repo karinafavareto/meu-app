5ab80c38050eb597e775a3c32c901bb8dd2bcbf2
Karina Favareto <karina.favareto@gmail.com>
2025-11-10 16:51:57 -0300

    Update Atv01.js

--- src/Atv01.js
+++ src/Atv01.js
@@ -2,14 +2,14 @@ import Relogio from "./Relogio";
 import Letreiro from './Letreiro';
 import { Link } from "react-router-dom";
 
-const Atv01 = () =>{
+const Atv01 = () => {
     return (
-        <>
-           <h1>Atividade 1</h1>
+        <div className="container d-flex flex-column align-items-center mt-5">
+           <h1 className="display-3 mb-5">Atividade 1</h1>
            <Relogio />
            <Letreiro />
-        <Link to="../">Voltar</Link>
-        </>
+        <Link to="../"><button type="button" className="btn btn-info mt-5">Voltar</button></Link>
+        </div>
     )
 }
 
