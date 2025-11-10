583c96bf2a16c30e97495ee84648ed7e846949ae
Karina Favareto <karina.favareto@gmail.com>
2025-11-10 16:53:24 -0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Home = () =>{
     return (
-        <div>
-            <h1>Página Inicial</h1>
+        <div className="container d-flex flex-column align-items-center mt-5">
+            <h1 className="display-3 mb-5">Página Inicial</h1>
             <nav>
-                <ul>
-                    <li>
+                <ul className="list-group">
+                    <li class="list-group-item list-group-item-action">
                         <Link to="/Atv01">Atividade 1</Link>
                     </li>
-                    <li>
+                    <li class="list-group-item list-group-item-action">
                         <Link to="/Atv02">Atividade 2</Link>
                     </li>
                 </ul>
