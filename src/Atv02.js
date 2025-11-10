65023e5262853bd9c1ba1bc60e99db548d638dba
Karina Favareto <karina.favareto@gmail.com>
2025-11-10 16:52:12 -0300

    Update Atv02.js

--- src/Atv02.js
+++ src/Atv02.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+import ContadorPessoas from "./ContadorPessoas";
 
-const Atv02 = () =>{
+const Atv02 = () => {
     return (
-        <div>
-           <h1>Atividade 2</h1>
+        <div className="container d-flex flex-column align-items-center mt-5">
+           <h1 className="display-3 mb-5">Atividade 2</h1>
+           <ContadorPessoas />
+        <Link to="../"><button type="button" className="btn btn-info mt-5">Voltar</button></Link>
         </div>
     )
 }
