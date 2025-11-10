24a094768f6c04932a44d1ae203c3a6ea04007ed
Karina Favareto <karina.favareto@gmail.com>
2025-11-10 16:52:46 -0300

    Update Relogio.js

--- src/Relogio.js
+++ src/Relogio.js
@@ -7,7 +7,7 @@ const Relogio = () => { // arrow function, omitindo as chaves {}, possui retorno
     useEffect(() => {
 
         const intervalId = setInterval(() => { setTime(new Date()); }, 1000); // setup / montagem
-                                                // função               // tempo
+                                        // função anônima              // tempo
         
         return () => { // cleanup / desmontagem | recebe a instrução de como parar a execução; é executada depois
             clearInterval(intervalId);
@@ -15,11 +15,11 @@ const Relogio = () => { // arrow function, omitindo as chaves {}, possui retorno
 
     }, [] ); // array de dependências, quando vazio, garante que o efeito só rode uma vez (no momento da montagem do componente)
 
-    return ( //retorna o jsx, a parte visual
-        <>
-            <h1>Hora atual</h1>
-            <p>{time.toLocaleTimeString()}</p>
-        </>
+    return ( // retorna o jsx, a parte visual
+        <div className="container d-flex flex-column align-items-center mt-5"> 
+            <h2>Hora atual</h2>
+            <h4>{time.toLocaleTimeString()}</h4>
+        </div>
         
     )
 }
