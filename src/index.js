fbe3c39e3b2be4dd9343b5be12501330017c7add
Karina Favareto <karina.favareto@gmail.com>
2025-11-10 16:53:08 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import MinhasRotas from './minhasRotas';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
