7e92aa8a1f1212bcb39eea008a59d68e852b8bef
Karina Favareto <karina.favareto@gmail.com>
2025-11-12 11:50:37 -0300

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -12,6 +12,13 @@ export default function Profile() {
   return (
     <Card>
         <h4>Meu Texto</h4>
+        <Avatar 
+          size={100}
+          person={{
+            name: 'Katsuko Saruhashi',
+            imageId: 'YfeOqp2'
+          }}
+          />
     </Card>
   );
 }
